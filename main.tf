c3296c4d19cca649ddd2290b1a42feb3e9b21f9c
Ankush More <ankushitguy@gmail.com>
2025-08-16 07:00:50 +0530

    terraform formatted using 'fmt'

--- main.tf
+++ main.tf
@@ -13,9 +13,9 @@ terraform {
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
   credentials = var.credentials
 }
 
